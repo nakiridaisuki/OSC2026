30d0a220a3fb2ccd2ccc7e960282825fe5632bfc
nakiridaisuki <nakiridaisuki5500@gmail.com>
2026-08-25 15:51:45 +0800

    lab 4: add trap task queue test

--- lab4/main.c
+++ lab4/main.c
@@ -42,6 +42,58 @@ void exec(void (*func)(void)) {
                  : "r"(user_sp));
 }
 
+void test_cb(void *args) {
+    int *data = (int *)args;
+    int id    = data[0];
+    int prio  = data[1];
+    printf("Test callback id %d with priority %d\n", id, prio);
+}
+
+void test_timer(void *args) { printf("Trigger.\n"); }
+
+void high_prio_task(void *args) {
+    printf("    [High Task] This is a high priority task.\n");
+    for (int i = 0; i < 100000; i++) {
+        asm volatile("nop");
+    }
+    printf("    [High Task] End.\n");
+}
+
+void low_prio_task(void *args) {
+    printf("[Low Task] This is a low priority task.\n");
+    printf("[Low Task] Calling a high priority task...\n");
+
+    trap_add_task(high_prio_task, NULL, 1);
+
+    // trigger task handling using timer intr.
+    Timer timer;
+    add_timer(&timer, 0, test_timer, NULL);
+
+    printf("[Low Task] End.\n");
+}
+
+void task_qeueu_test() {
+    int test1[2] = {1, 0};
+    int test2[2] = {2, 1};
+    int test3[2] = {3, 1};
+    int test4[2] = {4, 2};
+    printf("Add test task from 4 to 1\n");
+    trap_add_task(test_cb, test4, test4[1]);
+    trap_add_task(test_cb, test3, test3[1]);
+    trap_add_task(test_cb, test2, test2[1]);
+    trap_add_task(test_cb, test1, test1[1]);
+
+    // trigger task handling using timer intr.
+    Timer timer;
+    add_timer(&timer, 0, test_timer, NULL);
+
+    printf("Test Preemption\n");
+    trap_add_task(low_prio_task, NULL, 3);
+
+    // trigger task handling using timer intr.
+    add_timer(&timer, 0, test_timer, NULL);
+}
+
 int main(unsigned long hartid, const uint8_t *fdt_ptr) {
     init_trap();
     printf("Trap initialized.\n");
@@ -50,18 +102,18 @@ int main(unsigned long hartid, const uint8_t *fdt_ptr) {
     printf("PLIC initialized.\n");
 
     cpionewc_init_from_fdt(fdt_ptr);
-    printf("initrd start address: 0x%p\n", CPIO_START_ADDR);
+    printf("initrd start address: 0x%lx\n", CPIO_START_ADDR);
 
     init_malloc(fdt_ptr);
     printf("Malloc initialized\n");
 
     init_uart(fdt_ptr, true);
-    uart_putchar('T');
     printf("UART Initialized.\n");
 
     init_timer(fdt_ptr);
     printf("Timer initialized\n");
 
+    task_qeueu_test();
     // exec(fake_user);
 
     shell();
